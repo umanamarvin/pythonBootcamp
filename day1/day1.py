216b820357e4b7aa036c019adb8ca494f5929544
umanamarvin <124921407+umanamarvin@users.noreply.github.com>
2023-03-14 18:34:29 +0100

    Add files via upload

---
+++ day1/day1.py
@@ -0,0 +1,19 @@
+print("hello yo madafakas")
+
+#score = 1
+
+#print (f"this is the score {score}")
+
+print ("Next I will tell you how many time you have left if you live until you are 90 years old")
+age = input ("\nFirst tell how old are you???")
+
+time_left = 90 - int(age)
+
+months = time_left * 12
+weeks = time_left * 52
+days = time_left * 365
+
+print (f"\n
+You have {days} days, {weeks} weeks, {months} months left")
+
+
